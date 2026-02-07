b5e1052ed3fd396dc8759405355d7f39627310c5
Gian Carlo <giancarloandres41@gmail.com>
2026-02-07 16:43:52 +0800

    Add files via upload

---
+++ HelloGUI.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class HelloGUI {
+    public static void main(String giann[]) {
+        
+        JFrame frame = new JFrame("Login Form");
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null); 
+
+        JLabel userLabel = new JLabel("Username:");
+        userLabel.setBounds(20, 20, 80, 25);
+        frame.add(userLabel);
+
+        JTextField userText = new JTextField();
+        userText.setBounds(120, 20, 165, 25);
+        frame.add(userText);
+
+        JLabel passLabel = new JLabel("Password:");
+        passLabel.setBounds(20, 60, 80, 25);
+        frame.add(passLabel);
+
+        JPasswordField passText = new JPasswordField();
+        passText.setBounds(120, 60, 165, 25);
+        frame.add(passText);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(120, 100, 80, 25);
+        frame.add(loginButton);
+
+        frame.setVisible(true);
+    }
+}
